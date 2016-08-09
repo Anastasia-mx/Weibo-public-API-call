d0690f582f73a2788db03ea45d3013a9533c5f97
Anastasia-mx <anastasia@thresher.io>
2016-08-09 16:12:09 -0400

    Add files via upload

---
+++ Firehose!.py
@@ -0,0 +1,51 @@
+
+# coding: utf-8
+
+# In[1]:
+
+import urllib.request
+import json
+import pandas as pd
+#import requests
+
+
+# In[6]:
+
+token = '2.00lZ16UGJycrwB0112d5b3f1SOWegE'
+page = urllib.request.Request('https://api.weibo.com/2/statuses/public_timeline.json?access_token=' + token)
+response = urllib.request.urlopen(page)
+str_response = response.read().decode('utf-8')
+data = json.loads(str_response)
+df = pd.io.json.json_normalize(data['statuses'])
+
+df.head()
+
+dfingest = []
+
+dfingest = df.text[0:4]
+
+
+# In[ ]:
+
+#script that got the json but didn't put it anywhere
+#url = 'https://api.weibo.com/2/statuses/public_timeline.json'
+#params = {'source': '157679999', 'access_token': '2.00wPOUWGp79GsD434bad3a5b03nvE4', 'count': 50}
+
+# request public timeline
+#response = requests.get(url, params=params)
+#jres = response.json()
+#print(jres)
+
+
+#other things i tried that didn't work
+#rawreply = connection.response().read()
+#data = json.loads(rawreply.decode())
+
+#elevations = response.read()
+#data = json.loads(elevations)
+
+
+# In[ ]:
+
+
+
